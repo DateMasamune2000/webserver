abd2fad1cd0014366cec9e14d1403f177d47ed79
Varun Girimaji <vgirimaji@gmail.com>
2025-08-16 09:42:58 -0500

    Added second test case for escape characters

--- test/test.c
+++ test/test.c
@@ -21,6 +21,9 @@ struct route routes[MAXROUTES];
 
 int main(void)
 {
+	printf("[33m[1m*** LOADING ROUTES ***[0m\n\n");
+
+	printf("[1mTest 1: Basics[0m\n");
 	printf("Loading routes...\n");
 
 	assert(loadroutes("./routes.txt", routes) == 2);
@@ -32,7 +35,21 @@ int main(void)
 	assert(strcmp(routes[1].from, "/about") == 0);
 	assert(strcmp(routes[1].to, "about.txt") == 0);
 
-	printf("Routes loaded successfully.\n");
+	printf("[1m[[32mX[0m[1m][0m [32mPASS[0m\n\n");
+
+	printf("[1mTest 2: Escape[0m\n");
+	printf("Loading routes...\n");
+
+	assert(loadroutes("./routes2.txt", routes) == 2);
+
+	printf("Routes loaded.\nChecking...\n");
+
+	assert(strcmp(routes[0].from, "/") == 0);
+	assert(strcmp(routes[0].to, "test.c") == 0);
+	assert(strcmp(routes[1].from, "/ab%20out") == 0);
+	assert(strcmp(routes[1].to, "ab out.txt") == 0);
+
+	printf("[1m[[32mX[0m[1m][0m [32mPASS[0m\n\n");
 
 	return 0;
 }
